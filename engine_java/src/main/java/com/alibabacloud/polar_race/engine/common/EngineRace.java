2f3c445e9cf7167ae3796149eac092d92964330b
王霄霄 <1141195807@qq.com>
2018-10-27 22:01:33 +0800

    提测

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/common/EngineRace.java
@@ -10,11 +10,11 @@ import org.apache.log4j.Logger;
 public class EngineRace extends AbstractEngine {
 
 	private Logger logger = Logger.getLogger(EngineRace.class);
-	private LSMDB db;
+	private LSMTree db;
 
 	@Override
 	public void open(String path) throws EngineException {
-		db = new LSMDB(path);
+		db = new LSMTree(path);
 	}
 	
 	@Override
