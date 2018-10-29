545fdfceb9b38d78727622f71cecba63b7ffa309
王霄霄 <1141195807@qq.com>
2018-10-29 20:28:10 +0800

    提测

--- engine_java/src/main/java/com/alibabacloud/polar_race/engine/core/merge/Level0Merger.java
+++ engine_java/src/main/java/com/alibabacloud/polar_race/engine/core/merge/Level0Merger.java
@@ -49,8 +49,7 @@ public class Level0Merger extends Thread {
                     log.info("Start running level 0 merge thread at " + DateFormatter.formatCurrentDate());
                     log.info("Current queue size at level 0 is " + levelQueue0.size() + ", current free memory size: " + Runtime.getRuntime().freeMemory());
 
-                    List<BufferPoolMXBean> pools = ManagementFactory.getPlatformMXBeans(BufferPoolMXBean.class);  //查看堆外内存
-
+                    //List<BufferPoolMXBean> pools = ManagementFactory.getPlatformMXBeans(BufferPoolMXBean.class);  //查看堆外内存
 
                     long start = System.nanoTime();
                     LevelQueue levelQueue1 = levelQueueList.get(LSMDB.LEVEL1);
